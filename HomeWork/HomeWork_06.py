4d6e628308045f88bc76a89b53bb3beaa1052b43
riccardo24-7 <60128039+riccardo24-7@users.noreply.github.com>
2021-12-14 17:01:07 +0300

    refactoring code

--- HomeWork/HomeWork_06.py
+++ HomeWork/HomeWork_06.py
@@ -12,27 +12,27 @@ bot = telebot.TeleBot('')
 # function will be activate after user writes /start
 @bot.message_handler(commands=['start'])
 def start_bot(message):
-	bot.send_message(message.from_user.id, "Привет " + message.from_user.first_name + "!" + " Напиши /choose")
+	bot.send_message(message.from_user.id, 'Привет ' + message.from_user.first_name + '!' + ' Напиши /choose')
 
 # function will be activate after user writes /choose
 @bot.message_handler(commands=['choose'])
 def get_choise(message):
-    if message.text == "/choose":
+    if message.text == '/choose':
         markup_choose = types.ReplyKeyboardMarkup(row_width=2)
-        btn_web = types.KeyboardButton("Проверь сайт")
+        btn_web = types.KeyboardButton('Проверь сайт')
         btn_string = types.KeyboardButton('Проверь предложение')
         btn_calc = types.KeyboardButton('Калькулятор')
         markup_choose.add(btn_web, btn_string, btn_calc)
-        bot.send_message(message.chat.id, "Выбери действие:", reply_markup=markup_choose) 
+        bot.send_message(message.chat.id, 'Выбери действие:', reply_markup=markup_choose) 
         
         
 @bot.message_handler(content_types=['text'])
 def get_text_message(message):
     
-    if message.text == "Привет":
-        bot.send_message(message.from_user.id, "Привет " + message.from_user.first_name + "!" + " Напиши /choose")
+    if message.text == 'Привет':
+        bot.send_message(message.from_user.id, 'Привет ' + message.from_user.first_name + '!' + ' Напиши /choose')
              
-    elif message.text == "Проверь сайт":
+    elif message.text == 'Проверь сайт':
         
         keyboard_web = types.InlineKeyboardMarkup()
         key_google = types.InlineKeyboardButton(text='Google', callback_data='google')
@@ -41,18 +41,18 @@ def get_text_message(message):
         keyboard_web.add(key_google, key_yandex, key_yahoo)
         bot.send_message(message.from_user.id, text='Пока я могу проверить только 3 сайта, выбирай', reply_markup=keyboard_web) 
               
-    elif message.text == "Проверь предложение":
+    elif message.text == 'Проверь предложение':
         
-        msg = bot.send_message(message.from_user.id, "Введите предложение для анализа, либо отмена")
+        msg = bot.send_message(message.from_user.id, 'Введите предложение для анализа, либо отмена')
         bot.register_next_step_handler(msg, string_parsing)
         
-    elif message.text == "Калькулятор":
+    elif message.text == 'Калькулятор':
         markup_calc = types.ReplyKeyboardRemove(selective=True)
-        msg = bot.send_message(message.chat.id, "Введи два числа через пробел, либо отмена ", reply_markup=markup_calc)
+        msg = bot.send_message(message.chat.id, 'Введи два числа через пробел, либо отмена ', reply_markup=markup_calc)
         bot.register_next_step_handler(msg, calculator_activate)
         
     else:
-        bot.send_message(message.from_user.id, "Неизвестная компанда, введи /help")
+        bot.send_message(message.from_user.id, 'Неизвестная компанда, введи /help')
 
 # Парсер строки, для создания статистики о введенном предложении
 def string_parsing(message):
@@ -82,7 +82,7 @@ def string_parsing(message):
         token_dict = dict(Counter(token_words))
         
         most_occur_word = max(token_dict.items(), key = operator.itemgetter(1))[0]
-        max_len_key = max(findall(r"\b\w+\b", final_string), key = len)
+        max_len_key = max(findall(r'\b\w+\b', final_string), key = len)
         
         statistic = f'''Статистика предложения (без предлогов, союзов и знаков препинания)
         Длина предложения: {len(token_words)} слов
@@ -105,13 +105,13 @@ def calculator_activate(message):
     else:
         
         markup_calc = types.ReplyKeyboardMarkup(row_width=2)
-        btn_sum = types.KeyboardButton("Сложить")
+        btn_sum = types.KeyboardButton('Сложить')
         btn_minus = types.KeyboardButton('Отнять')
         btn_multiply = types.KeyboardButton('Умножить')
         btn_divide = types.KeyboardButton('Делить')
         markup_calc.add(btn_sum, btn_minus, btn_multiply, btn_divide)
 
-        msg = bot.send_message(message.chat.id, " Выбери действие:", reply_markup=markup_calc)
+        msg = bot.send_message(message.chat.id, 'Выбери действие:', reply_markup=markup_calc)
         bot.register_next_step_handler(msg, calculator_operations)
 
 # Далее опредееление операции      
@@ -121,53 +121,53 @@ def calculator_operations(message):
     
     count_list = user_num.split()
     
-    if user_numbers == "Сложить":
+    if user_numbers == 'Сложить':
         result = int(count_list[0]) + int(count_list[1])
-        bot.send_message(message.from_user.id, "Результат сложения: " + str(result))
+        bot.send_message(message.from_user.id, 'Результат сложения: ' + str(result))
         
-    elif user_numbers == "Отнять":
+    elif user_numbers == 'Отнять':
         result = int(count_list[0]) - int(count_list[1])
-        bot.send_message(message.from_user.id, "Результат разности: " + str(result))
+        bot.send_message(message.from_user.id, 'Результат разности: ' + str(result))
         
-    elif user_numbers == "Умножить":
+    elif user_numbers == 'Умножить':
         result = int(count_list[0]) * int(count_list[1])
-        bot.send_message(message.from_user.id, "Результат умножения: " + str(result))
+        bot.send_message(message.from_user.id, 'Результат умножения: ' + str(result))
         
-    elif user_numbers == "Делить":
+    elif user_numbers == 'Делить':
         if int(count_list[1]) != 0:     
             result = round(int(count_list[0]) / int(count_list[1]),2)
-            bot.send_message(message.from_user.id, "Результат деления: " + str(result))
+            bot.send_message(message.from_user.id, 'Результат деления: ' + str(result))
             
         else:
-            bot.send_message(message.from_user.id, "Не ломай мне бота")
+            bot.send_message(message.from_user.id, 'Не ломай мне бота')
             
     markup_calc = types.ReplyKeyboardRemove(selective=True)
-    msg = bot.send_message(message.chat.id, "Введи два числа через пробел: ", reply_markup=markup_calc)
+    msg = bot.send_message(message.chat.id, 'Введи два числа через пробел: ', reply_markup=markup_calc)
     bot.register_next_step_handler(msg, calculator_activate)          
 
 @bot.callback_query_handler(func=lambda call: True)
 def callbackFunction(call):
     
-    if call.data == "google": 
+    if call.data == 'google': 
         r = requests.get('https://www.google.ru')
         if r.status_code == 200:   
-            bot.send_message(call.message.chat.id, "Сайт google доступен")
+            bot.send_message(call.message.chat.id, 'Сайт google доступен')
         else:
-            bot.send_message(call.message.chat.id, "Сайт не доступен")
-    elif call.data == "yandex":
+            bot.send_message(call.message.chat.id, 'Сайт не доступен')
+    elif call.data == 'yandex':
         r = requests.get('https://www.yandex.ru')
         if r.status_code == 200:   
-            bot.send_message(call.message.chat.id, "Сайт yandex доступен")
+            bot.send_message(call.message.chat.id, 'Сайт yandex доступен')
         else:
-            bot.send_message(call.message.chat.id, "Сайт не доступен")
-    elif call.data == "yahoo":
+            bot.send_message(call.message.chat.id, 'Сайт не доступен')
+    elif call.data == 'yahoo':
         r = requests.get('https://www.yahoo.com')
         if r.status_code == 200:   
-            bot.send_message(call.message.chat.id, "Сайт yahoo доступен")
+            bot.send_message(call.message.chat.id, 'Сайт yahoo доступен')
         else:
-            bot.send_message(call.message.chat.id, "Сайт не доступен")
+            bot.send_message(call.message.chat.id, 'Сайт не доступен')
     else:
-        bot.send_message(call.message.chat.id, "Какая-то ошибка") 
+        bot.send_message(call.message.chat.id, 'Какая-то ошибка') 
 
 bot.polling(none_stop=True, interval=0)
 
